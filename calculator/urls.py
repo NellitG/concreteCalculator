bd9827c323bb9852fec53dbe0d297145af8523e0
NellitG <kamaunelson157@gmail.com>
2025-05-06 18:05:50 +0300

    updated URL to input fencing calculator path

--- calculator/urls.py
+++ calculator/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import fencing_calculator
 
 urlpatterns = [
     path('', views.fencing, name='fencing'),  # URL for the main fencing calculator page
+    path('', fencing_calculator, name='fencing_calculator'),  # URL for the calculator form
 ]
